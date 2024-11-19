6d73afc271145cde059fa4204f79272a0ae62fe0
Pedro Costa <phtcosta@gmail.com>
2024-11-19 13:15:47 -0300

    script to check execution status

--- rv-android/execution_status.py
+++ rv-android/execution_status.py
@@ -6,10 +6,13 @@ from datetime import timedelta, datetime
 import utils
 from experiment.memory import Memory
 import os
-from experiment.task import DEFAULT_DATETIME
-
+from experiment.task import Task, DEFAULT_DATETIME
+import log.logcat_parser as parser
+from constants import EXTENSION_LOGCAT
 
 def status(memory_file: str) -> dict:
+    results_dir = os.path.dirname(memory_file)
+    print(f"results_dir={results_dir}")
     try:
         memory = Memory.read(memory_file)
     except Exception as e:
@@ -22,7 +25,7 @@ def status(memory_file: str) -> dict:
     pct = round((completed_tasks / total_tasks) * 100, 2) if total_tasks else 0.0
 
     apks = get_apks(memory)
-    tasks = get_tasks(memory)
+    tasks = get_tasks(memory, results_dir)
 
     return {"total": total_tasks, "completed": completed_tasks, "pct": pct, "errors": errors, "tasks": tasks, "apks": apks}
 
@@ -50,12 +53,12 @@ def get_apks(memory: Memory):
     return apks
 
 
-def get_tasks(memory: Memory):
+def get_tasks(memory: Memory, results_dir: str):
     tasks = []
     for task in memory.tasks:
-        time: float = 0.0
+        time_tmp: float = 0.0
         if task.executed:
-            time = (task.finish_time - task.start_time).total_seconds()
+            time_tmp = (task.finish_time - task.start_time).total_seconds()
         start = ""
         if task.start_time != DEFAULT_DATETIME:
             start = utils.datetime_to_string(task.start_time)
@@ -67,14 +70,34 @@ def get_tasks(memory: Memory):
             "executed": task.executed,
             "start": start,
             "finish": finish,
-            "time": time
+            "time": time_tmp,
+            "errors": get_errors(task, results_dir)
         })
     return tasks
 
 
+def get_errors(task: Task, results_dir: str):
+    errors = []
+    if not task.executed:
+        return errors
+    base_name = "{0}__{1}__{2}__{3}".format(task.apk, task.repetition, task.timeout, task.tool)
+    apk_results_dir = os.path.join(results_dir, task.apk)
+    task.logcat_file = os.path.join(apk_results_dir, "{}{}".format(base_name, EXTENSION_LOGCAT))
+    rvsec_errors, _ = parser.parse_logcat_file(task.logcat_file)
+    for error in rvsec_errors:
+        errors.append({
+            "msg": error.unique_msg,
+            "time_occurred": utils.datetime_to_string(error.time_occurred),
+            "time_since_task_start": str(error.time_occurred - task.start_time)
+        })
+    return errors
+
+
+
+
 if __name__ == '__main__':
     # memory_file_path = sys.argv[1]
-    memory_file_path = "/pedro/desenvolvimento/workspaces/workspaces-doutorado/workspace-rv/rvsec/rv-android/results/20241119090210/execution_memory.json"
+    memory_file_path = "/pedro/desenvolvimento/workspaces/workspaces-doutorado/workspace-rv/rvsec/rv-android/results/20241119111618/execution_memory.json"
     if os.path.exists(memory_file_path):
         data = status(memory_file_path)
         print(json.dumps(data, indent=2))

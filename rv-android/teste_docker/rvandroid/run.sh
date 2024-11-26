a5ff13ed3b7a79935f4987a634ac80dfde8accee
Pedro Costa <phtcosta@gmail.com>
2024-11-26 11:58:06 -0300

    incrementing docker image

--- rv-android/teste_docker/rvandroid/run.sh
+++ rv-android/teste_docker/rvandroid/run.sh
@@ -1,6 +1,21 @@
 #!/bin/env bash
 
-docker run -it --rm --device /dev/kvm -e PARTITION=24576 -e MEMORY=6144 -e CORES=2 --name rvsec-01 phtcosta/rvandroid:0.0.1
+EXPERIMENT02_BASE_DIR=/home/pedro/desenvolvimento/RV_ANDROID/EXPERIMENTO_02/BASE
+APKS_DIR=$EXPERIMENT02_BASE_DIR/apks/tmp
+INSTRUMENTED_DIR=$EXPERIMENT02_BASE_DIR/instrumented
+RESULTS_DIR=$EXPERIMENT02_BASE_DIR/results
 
+echo "APKS_DIR=${APKS_DIR}"
+
+#docker run -it --rm --device /dev/kvm -e PARTITION=24576 -e MEMORY=6144 -e CORES=2 --name rvsec-01 phtcosta/rvandroid:0.0.1
+#docker run -it --rm --entrypoint "/bin/bash" --device /dev/kvm -e PARTITION=24576 -e MEMORY=6144 -e CORES=2 --name rvsec-01 phtcosta/rvandroid:0.0.1
+#docker run -it --rm --entrypoint "/bin/bash" --device /dev/kvm -v $APKS_DIR:/opt/rvsec/rv-android/apks -e PARTITION=24576 -e MEMORY=6144 -e CORES=2 --name rvsec-01 phtcosta/rvandroid:0.0.1
+
+docker run -it --rm --device /dev/kvm --entrypoint "/bin/bash" --name rvsec-01 \
+-v $APKS_DIR:/opt/rvsec/rv-android/apks \
+-v $INSTRUMENTED_DIR:/opt/rvsec/rv-android/out \
+-v $RESULTS_DIR:/opt/rvsec/rv-android/results \
+-e MEMORY=6144 -e CORES=2 \
+phtcosta/rvandroid:0.0.1
 
 #docker run -d --device /dev/kvm -p 5555:5555 -v androiddata:/data -e PARTITION=24576 -e MEMORY=6144 -e CORES=2 --name docker-android-emulator cndaqiang/docker-android-emulator:api-33
--- rv-android/teste_docker/tools/run.sh
+++ rv-android/teste_docker/tools/run.sh
@@ -1,6 +1,36 @@
 #!/bin/env bash
 
-docker run -it --rm --device /dev/kvm -e PARTITION=24576 -e MEMORY=6144 -e CORES=2 --name rvsec-tool phtcosta/rvandroid_tools:0.0.1
+#docker run -it --rm --device /dev/kvm -e PARTITION=24576 -e MEMORY=6144 -e CORES=2 --name rvsec-tool phtcosta/rvandroid_tools:0.0.1
 
+
+#docker run -d --device /dev/kvm -p 5555:5555 -v androiddata:/data -e PARTITION=24576 -e MEMORY=6144 -e CORES=2 --name docker-android-emulator cndaqiang/docker-android-emulator:api-33
+
+#!/bin/env bash
+
+EXPERIMENT02_BASE_DIR=/home/pedro/desenvolvimento/RV_ANDROID/EXPERIMENTO_02/BASE
+APKS_DIR=$EXPERIMENT02_BASE_DIR/apks/tmp
+INSTRUMENTED_DIR=$EXPERIMENT02_BASE_DIR/instrumented
+RESULTS_DIR=$EXPERIMENT02_BASE_DIR/results
+
+echo "APKS_DIR=${APKS_DIR}"
+
+#docker run -it --rm --device /dev/kvm -e PARTITION=24576 -e MEMORY=6144 -e CORES=2 --name rvsec-01 phtcosta/rvandroid_tools:0.0.1
+#docker run -it --rm --entrypoint "/bin/bash" --device /dev/kvm -e PARTITION=24576 -e MEMORY=6144 -e CORES=2 --name rvsec-01 phtcosta/rvandroid:0.0.1
+
+docker run -it --rm --device /dev/kvm -e PARTITION=24576 -e MEMORY=6144 -e CORES=2 --name rvsec_teste-01 phtcosta/rvandroid_tools:0.0.1
+#docker run -it --rm --entrypoint "/bin/bash" --device /dev/kvm -e PARTITION=24576 -e MEMORY=6144 -e CORES=2 --name rvsec_teste-01 phtcosta/rvandroid_tools:0.0.1
+
+#venv/bin/python main.py
+
+#docker run -it --rm --device /dev/kvm --name rvsec_teste-01 \
+#-e MEMORY=6144 -e CORES=2 \
+#phtcosta/rvandroid_tools:0.0.1
+
+#docker run -it --rm --device /dev/kvm --name rvsec_teste-01 \
+##-v $APKS_DIR:/opt/rvsec/rv-android/apks \
+##-v $INSTRUMENTED_DIR:/opt/rvsec/rv-android/out \
+##-v $RESULTS_DIR:/opt/rvsec/rv-android/results \
+#-e MEMORY=6144 -e CORES=2 \
+#phtcosta/rvandroid_tools:0.0.1
 
 #docker run -d --device /dev/kvm -p 5555:5555 -v androiddata:/data -e PARTITION=24576 -e MEMORY=6144 -e CORES=2 --name docker-android-emulator cndaqiang/docker-android-emulator:api-33

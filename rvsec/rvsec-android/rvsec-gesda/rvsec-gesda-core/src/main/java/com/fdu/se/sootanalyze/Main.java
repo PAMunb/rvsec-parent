eda0a9ed6a362ad737e02f4efac1e8d2541bc92b
Pedro Costa <phtcosta@gmail.com>
2025-01-06 09:56:28 -0300

    including method for testing locally

--- rvsec/rvsec-android/rvsec-gesda/rvsec-gesda-core/src/main/java/com/fdu/se/sootanalyze/Main.java
+++ rvsec/rvsec-android/rvsec-gesda/rvsec-gesda-core/src/main/java/com/fdu/se/sootanalyze/Main.java
@@ -20,7 +20,7 @@ import br.unb.cic.rvsec.apk.model.AppInfo;
 public class Main {
 	private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-	public static void main(String[] args) {
+	private static void runCLI(String[] args) {
 		CommandLineArgs jArgs = new CommandLineArgs();
 		JCommander jc = JCommander.newBuilder().addObject(jArgs).build();
 
@@ -31,12 +31,20 @@ public class Main {
 
 		jc.parse(args);
 
-		String androidPlatformsDir = jArgs.getAndroidDir();
-		String rtJarPath = jArgs.getRtJar();
-		String apk = jArgs.getApk();
-		String outputFile = jArgs.getOutputFile();
-		boolean debug = jArgs.isDebug();
+		executeAnalysis(jArgs.getAndroidDir(), jArgs.getRtJar(), jArgs.getApk(), jArgs.getOutputFile(), jArgs.isDebug());
+	}
 
+	private static void runLocal() {
+		String androidPlatformsDir = "/home/pedro/desenvolvimento/aplicativos/android/sdk/platforms";
+		String rtJarPath = "/home/pedro/.sdkman/candidates/java/8.0.302-open/jre/lib/rt.jar";
+		String apk = "/pedro/desenvolvimento/workspaces/workspaces-doutorado/workspace-rv/rvsec/rv-android/apks_examples/cryptoapp.apk";
+		String outputFile = "/home/pedro/tmp/rvsec-gesda.json";
+		boolean debug = false;
+
+		executeAnalysis(androidPlatformsDir, rtJarPath, apk, outputFile, debug);
+	}
+
+	private static void executeAnalysis(String androidPlatformsDir, String rtJarPath, String apk, String outputFile, boolean debug) {
 		if (debug) {
 			ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
 			root.setLevel(ch.qos.logback.classic.Level.DEBUG);
@@ -55,7 +63,12 @@ public class Main {
 			e.printStackTrace();
 		}
 		log.info("Analysis completed");
-
 	}
 
+	public static void main(String[] args) {
+		runCLI(args);
+
+//		runLocal();
+	}
+	
 }
